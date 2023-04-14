4b473fea16d6911a7cf0346bd89994a8d2baa90a
CarlosLopezLg <20170032@uttcampus.edu.mx>
2023-04-13 19:19:15 -0600

    Qr web sockets

--- app/Controllers/Http/Helpers/QRController.ts
+++ app/Controllers/Http/Helpers/QRController.ts
@@ -1,19 +1,31 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 var QRCode = require('qrcode')
+// import Ws from 'App/Services/Ws'
 
 
 export default class MailController {
     public async generateQR({ view, auth }: HttpContextContract){
-        console.log("Generando QR...")
-        /* QRCode.toString('123456',{type:'terminal'}, function (_err, url) {
-            console.log(url)
-        }) */
         const content = auth.user?.email
         const qrCode = await QRCode.toDataURL(content, {scale: 10})
         const data = {
-            qrCode: qrCode
+            qrCode: qrCode,
+            email: auth.user?.email
         }
-        console.log(auth.user?.email)
         return view.render('pages/scan_qr', data)
+    }
+
+    public async submitQR({ response, auth }: HttpContextContract) {
+        const user = await User.findByOrFail('email', auth.user!.email)
+        user.verified = true;
+        await user.save()
+        return response.redirect('/welcome')
+    }
+
+    public async forceQr({ request }: HttpContextContract) {
+        const email = request.input('email')
+        console.log(email)
+        return "Obsoleto"
+        //Ws.io.emit(`verificate-${email}`, { my: 'data' })
     }
 }
