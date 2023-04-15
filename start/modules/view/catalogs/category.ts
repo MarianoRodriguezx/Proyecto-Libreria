f3446b6b6287dd57c0101c25820605e18bee22f4
MarianoRodriguezx <marianoroddhd@gmail.com>
2023-04-14 19:56:24 -0600

    [ ADD ]  Views of Categories

--- start/modules/view/catalogs/category.ts
+++ start/modules/view/catalogs/category.ts
@@ -3,6 +3,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
     Route.resource('categories', 'CategoriesController')    
     Route.get('get/active/categories', 'CategoriesController.getActiveCategories')
+    
 })
 .namespace('App/Controllers/Http/Catalogs')
 .middleware(['auth'])
